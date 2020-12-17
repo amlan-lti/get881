03f8c1df431e239ac6d464202b1b710e087297aa
Amlan Sahoo <amlan.sahoo@lntinfotech.com>
2020-12-17 17:21:33 +0530

    new function

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,4 +4,8 @@ public class Calc {
 	public void add(int n1, int n2) {
 		System.out.println("Sum="+(n1+n2));
 	}
+	
+	public void diff(int n1, int n2) {
+		System.out.println("Difference="+(n1-n2));
+	}
 }
